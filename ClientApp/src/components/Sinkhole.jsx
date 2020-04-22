035fc32e432203b63b2778d2d20d2d406bf1a32a
Rob Kilduff <rkilduff88@gmail.com>
2020-04-22 13:59:34 -0400

    map on sinkholejsx and popup

--- ClientApp/src/components/Sinkhole.jsx
+++ ClientApp/src/components/Sinkhole.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react'
 import axios from 'axios'
-import ReactMapGL, { Marker, Popup } from 'react-map-gl'
+import ReactMapGL, {
+  Marker,
+  Popup,
+  GeolocateControl,
+  NavigationControl,
+} from 'react-map-gl'
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons'
 // import SinkholeAverageRating from './SinkholeAverageRating'
@@ -8,13 +13,19 @@ import PlaceSinkhole from '../images/placeholder_sinkhole.jpg'
 
 const Sinkhole = props => {
   const { sinkhole } = props
-  // const [viewport, setViewport] = useState({
-  //   width: 400,
-  //   height: 400,
-  //   latitude: this.prop.latitude,
-  //   longitude: this.prop.longitude,
-  //   zoom: 8,
-  // })
+  const [viewport, setViewport] = useState({
+    height: 400,
+    width: 400,
+    // need to change width
+    latitude: sinkhole.latitude,
+    longitude: sinkhole.longitude,
+    zoom: 12,
+  })
+  const [showPopup, setShowPopup] = useState(false)
+  const markerClicked = sinkhole => {
+    console.log('sinkhole clicked', sinkhole)
+    setShowPopup(true)
+  }
   // const [markers, setMarkers] = useState([])
 
   // const [newReviewText, setNewReviewText] = useState('')
@@ -50,7 +61,7 @@ const Sinkhole = props => {
     <main className="sinkhole-details">
       <img src={PlaceSinkhole} alt="Huh" />
       <section className="sinkhole-header">
-        <h1>Details for {sinkhole.name}</h1>
+        <h2>Details for {sinkhole.name}</h2>
 
         {/* <button className="directions-link">
           <FontAwesomeIcon icon={faMapMarkedAlt} />
@@ -63,9 +74,51 @@ const Sinkhole = props => {
         {/* <p className="reviews">
           <SinkholeAverageRating reviews={reviews} />
         </p> */}
-        <p className="address">
-          Address: {sinkhole.fullAddress} {sinkhole.county}
-        </p>
+        <p className="address">Address: {sinkhole.fullAddress}</p>
+        <p>County: {sinkhole.county}</p>
+        <section className="map-container">
+          <h2>{sinkhole.name} location</h2>
+          <ReactMapGL
+            {...viewport}
+            onViewportChange={setViewport}
+            mapboxApiAccessToken={
+              'pk.eyJ1IjoicmtpbGR1ZmYiLCJhIjoiY2s4czZna2lxMDFweDNsbzlyMmU0Ym50byJ9.DW5QkRiAEI4c4dEfA2eHyw'
+            }
+          >
+            <GeolocateControl
+              positionOptions={{ enableHighAccuracy: true }}
+              trackUserLocation={true}
+            ></GeolocateControl>
+            >
+            {showPopup && (
+              <Popup
+                latitude={sinkhole.latitude}
+                longitude={sinkhole.longitude}
+                closeButton={true}
+                closeOnClick={false}
+                onClose={() => setShowPopup(false)}
+                offsetTop={-5}
+              >
+                <div className="popup-window">🚩{sinkhole.name}</div>
+              </Popup>
+            )}
+            <Marker
+              latitude={sinkhole.latitude}
+              longitude={sinkhole.longitude}
+              key={sinkhole.id}
+              name={sinkhole.name}
+            >
+              <div onClick={() => markerClicked(sinkhole)}>🕳</div>
+              {/* <div>🕳</div> */}
+            </Marker>
+            <NavigationControl />
+          </ReactMapGL>
+        </section>
+        <section className="review">
+          <p>Is this a sinhole?</p>
+          <button>Yes!</button>
+          <button>Fake News!</button>
+        </section>
       </section>
     </main>
   )
